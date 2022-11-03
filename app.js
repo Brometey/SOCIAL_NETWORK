dc4411b7eb2d8da86809d8a0a406aa3679da5ae6
= <=>
2022-11-03 13:33:01 +0600

    removed ip

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const port = process.env.PORT || 8000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, '192.168.88.73', () => {
+    app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
   } catch (error) {
